e380628c9a0dee29daa50508632ab4424c6aa7e0
keyband <keybpr@gmail.com>
2020-12-13 15:34:51 -0300

    Added music to the game

--- global.gd
+++ global.gd
@@ -1,9 +1,13 @@
 extends Node
 signal sPlayerDead
 signal sPlayerWon
+
+const music = preload("res://scenes/music.tscn")
 func _ready():
 	OS.window_size*=4
-	
+func playMusic():
+	var i=music.instance()
+	add_child(i)
 func playerOnKing():
 	print_debug("Eba!")
 
